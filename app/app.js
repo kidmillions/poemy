3aaf2eb0fbe11cb9628069d7dc9632833467b61e
Chris C <claymanchris@gmail.com>
2015-01-28 17:29:24 +0800

    poem completed function

--- app/app.js
+++ app/app.js
@@ -2,33 +2,46 @@
 	var app = angular.module('poemy', []);
 	app.controller('PoemController', function() {
 		this.poems = samplePoems;
+		
 	});
 
 	app.controller("LineController", function() {
+		function completePoem(poem) {
+			if (poem.numLines > poem.lines.length) {
+				poem.complete = false;
+			} else {
+				poem.complete = true;
+			}
+		};
 		this.newLine = "";
 		this.addNewLine = function(poem) {
 			poem.lines.push(this.newLine);
 			this.newLine = "";
+			completePoem(poem);
 		}
 	});
 
 	var samplePoems = [{
 		title: "Sample Poem",
+		numLines: 3,
 		type: "Haiku",
 		lines: ["this is a first line,", "But the second is best,", "and the third is last."],
 		complete: true
 	}, {
 		title: "Poem 2",
+		numLines: 3,
 		type: "Haiku",
 		lines: ['An old silent pond...', 'A frog jumps into the pond,'],
 		complete: false
 	}, {
 		title: "Poem 3",
+		numLines: 3,
 		type: "Haiku",
 		lines: ["Light of the moon"],
 		complete: false
 	}, {
-		title: "Poem 4	",
+		title: "Poem 4",
+		numLines: 3,
 		type: "Haiku",
 		lines: ["A cricket disturbed", "the sleeping child; on the porch", "a man smoked and smiled."],
 		complete: true
